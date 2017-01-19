54c10e7c8a17877f5a90f084e5d2b54ce7282a58
sunnyshrestha <dev.suncha@gmail.com>
2017-01-19 16:44:18 +0545

    Initial commit

---
+++ app/src/main/java/dev/suncha/whatthechuck/ChuckNorrisFactModel.java
@@ -0,0 +1,44 @@
+package dev.suncha.whatthechuck;
+
+/**
+ * Created by nepahima on 1/19/2017.
+ */
+
+public class ChuckNorrisFactModel {
+    String icon_url;
+    String id;
+    String url;
+    String value;
+
+    public String getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(String icon_url) {
+        this.icon_url = icon_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
